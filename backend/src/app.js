0a86555e221e9df06fe6625ff8abc4e643b03a1c
triggereddown <deepmoitra2@gmail.com>
2026-02-05 16:20:24 +0530

    Connected backend to frontend of notes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import notesModel from "./models/notes.model.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const notes = [
   // title:"Title 1",
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const getNotes = async () => {
-    const res = await axios.get("http://localhost:3000/notes");
-    console.log(res);
+    const res = await axios.get("http://localhost:3000/api/notes");
+    console.log(res.data.notes);
+    setNotes(res.data.notes);
   };
+
+  useEffect(() => {
+    getNotes();
+  }, []);
 
   return (
     <div>
-      <div className="maindiv bg-amber-500"></div>
+      <div className="maindiv bg-gray-500 p-4 grid grid-cols-3 gap-6">
+        {notes.map((notes, idx) => {
+          return (
+            <div className="bg-amber-950 p-4 rounded " key={idx}>
+              <div className="title bg-amber-300 p-2 rounded mb-2">
+                {notes.title}
+              </div>
+              <div className="desc bg-amber-400 p-2 rounded">
+                {notes.description}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
